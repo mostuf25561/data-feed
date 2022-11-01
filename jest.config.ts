5f226c3a332a3b929c7aa9d76725c1495ce055ea
ofer shaham <ofer@sergata.com>
2022-11-01 13:28:25 +0200

    add jest config and typescript support

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+"use strict";
+export default {
+  // stop after first failing test
+  bail: true,
+  testMatch: ["**/*.test.[jt]s"],
+  moduleNameMapper: { "^jest/(.*)$": "<rootDir>/jest/$1" },
+  moduleDirectories: ["node_modules", "src"],
+  coverageDirectory: "src/__tests__/coverage",
+};
